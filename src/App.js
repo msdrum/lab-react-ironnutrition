e36dfde495d6790850ef41ec7f22933772a0747d
Marcio Silva <marciosilvadrum@gmail.com>
2022-11-09 15:07:26 -0300

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
 
 function App() {
-  return <div className="App"></div>;
+  const [allFoods, setAllFoods] = useState(foods);
+
+  return (
+    <div className="App">
+      {allFoods.map((food) => {
+        return (
+          <div className="lista-comidas">
+            <p> {food.name} </p>
+            <img src={food.image} alt="food" width={150} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
